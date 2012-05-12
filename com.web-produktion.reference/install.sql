52cf9f4c3081d59037d9997e71ce201b732340ad
Siora <siora@web-produktion.com>
2012-05-12 15:46:03 +0200

    some sql fixes

--- com.web-produktion.reference/install.sql
+++ com.web-produktion.reference/install.sql
@@ -8,17 +8,15 @@ CREATE TABLE IF NOT EXISTS wcf1_reference (
 	domain VARCHAR(255) NOT NULL DEFAULT 'http://',
 	public TINYINT(1) NOT NULL DEFAULT 0,
 	position INT(10) NOT NULL DEFAULT 0,
-	time INT(16) NOT NULL DEFAULT 0,
-	PRIMARY KEY (referenceID)
+	time INT(16) NOT NULL DEFAULT 0
 );
 
 -- TODO: need this ? or use com.woltlab.wcf.attachment
 -- wcf1_reference_screenshot
 DROP TABLE IF EXISTS wcf1_reference_screenshot;
 CREATE TABLE IF NOT EXISTS wcf1_reference_screenshot (
-	screenshotID INT(10) NOT NULL AUTO_INCREMENT,
+	screenshotID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	referenceID INT(10) NOT NULL,
 	sortOrder INT(10) NOT NULL,
-	fileExtension VARCHAR(50) NOT NULL,
-	PRIMARY KEY (screenshotID)
+	fileExtension VARCHAR(50) NOT NULL
 );
